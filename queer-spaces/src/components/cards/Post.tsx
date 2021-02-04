fa8080a2960913d98693ad486da2e5016403b410
realjasomo <jason.cramer6@gmail.com>
2021-02-04 18:29:02 -0500

    Added menu to posts that the user owns

--- queer-spaces/src/components/cards/Post.tsx
+++ queer-spaces/src/components/cards/Post.tsx
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
 import { firebase, usersRef, usernameRef } from '../'
-import { IconButton } from '@material-ui/core'
+import { IconButton, Menu, MenuItem } from '@material-ui/core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCommentDots } from '@fortawesome/free-regular-svg-icons'
 import ThumbUpOutlinedIcon from '@material-ui/icons/ThumbUpOutlined'
 import ThumbDownOutlinedIcon from '@material-ui/icons/ThumbDownOutlined'
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 import BlankProfile from '../../res/bp.png'
 import styles from '../../css/postbox.module.css'
 import { User } from '../../interfaces/User'
 import { generate, AnonymousInfo } from '../../util/AnonymousGenerator'
-interface PostPropState{
+
+interface PostProp {
     category: string,
     text_content?: string | null,
     poll_question?: string | null,
@@ -19,11 +21,24 @@ interface PostPropState{
     created: firebase.firestore.Timestamp,
     userInfo?: Partial<User>
 }
-export default class Post extends Component<PostPropState, PostPropState> {
+
+interface PostPropState{
+    category: string,
+    text_content?: string | null,
+    poll_question?: string | null,
+    poll_options?: Array<string> | null,
+    image_url?: string | null,
+    user_id?: string | null,
+    created: firebase.firestore.Timestamp,
+    userInfo?: Partial<User>,
+    menuOpen: HTMLButtonElement | null,
+}
+export default class Post extends Component<PostProp, PostPropState> {
     constructor(props: PostPropState){
         super(props);
         this.state = {
-            ...props
+            ...props,
+            menuOpen: null
         }
         this.updateUserInfo();
     }
@@ -53,6 +68,16 @@ export default class Post extends Component<PostPropState, PostPropState> {
     render() {
         var anonymous : AnonymousInfo = generate();
         return (<>
+            <Menu
+                id="post-menu"
+                style={{top: '40px'}}
+                anchorEl={this.state.menuOpen}
+                keepMounted
+                open={!!this.state.menuOpen}
+                onClose={() => this.setState({menuOpen: null})}>
+                <MenuItem>Edit</MenuItem>
+                <MenuItem>Delete</MenuItem>
+            </Menu>
             <div className={styles.card}>
                 <div id="profile" className={styles.profile}>
                     <img style={{backgroundColor: anonymous.color}}src={this.state.userInfo?.photo || anonymous.image || BlankProfile} className = {styles.profileImage} alt="profile"/> 
@@ -60,6 +85,7 @@ export default class Post extends Component<PostPropState, PostPropState> {
                         <h2 style={{fontFamily:'roboto', color: '#5A5353'}}>{this.state.userInfo?.name || anonymous.name}</h2>
                         <p style={{fontFamily:'roboto', color: '#D8D8D8'}}>{this.state.userInfo?.username || this.state.userInfo?.email || anonymous.username}</p>
                     </div>
+                    <div style={{marginLeft:'auto', marginRight:'15px'}}>{firebase.auth().currentUser?.uid === this.state.user_id ?<IconButton onClick={(event) => this.setState({menuOpen: event.currentTarget})}><MoreHorizIcon className={styles.iconBlack}/></IconButton>:<></>}</div>
                 </div> 
                 <div className={styles.postText}>
                     {this.state.text_content}
@@ -84,7 +110,7 @@ export default class Post extends Component<PostPropState, PostPropState> {
                 </div>
             </div>
             <div>
-               <pre>{JSON.stringify(this.state)}</pre>
+               <pre>{JSON.stringify(this.props)}</pre>
             </div>
             </>)
     }
