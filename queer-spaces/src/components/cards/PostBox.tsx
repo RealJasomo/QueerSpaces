12f618378be1291e9bff7acc8d01a9acdf1a1564
realjasomo <jason.cramer6@gmail.com>
2021-02-14 04:40:25 -0500

    Image upload complete for postbox

--- queer-spaces/src/components/cards/PostBox.tsx
+++ queer-spaces/src/components/cards/PostBox.tsx
@@ -1,19 +1,21 @@
 import React, { Component } from 'react'
-import {IconButton, TextField } from '@material-ui/core'
+import {IconButton, TextField , Button, Modal} from '@material-ui/core'
 import Autocomplete from '@material-ui/lab/Autocomplete'
 import BlankProfile from '../../res/bp.png'
 import styles from '../../css/postbox.module.css'
 import InsertChartIcon from '@material-ui/icons/InsertChart'
 import ImageIcon from '@material-ui/icons/Image'
 import SendIcon from '@material-ui/icons/Send'
-import {firebase} from '../';
+import {firebase, storageRef} from '../';
 import {categories} from '../../util/Constants'
+import {v4 as uuid} from 'uuid'
 
 interface PostBoxProps {
 
 }
 
 interface PostBoxState {
+    openImageModal: boolean,
     categories: Array<String>,
     category: String | null,
     content: string | null,
@@ -26,10 +28,11 @@ interface PostBoxState {
 }
 
 export default class PostBox extends Component<PostBoxProps, PostBoxState>{
-    
+    fileUpload: HTMLInputElement | null
     constructor(props: PostBoxProps){
         super(props);
         this.state = {
+            openImageModal: false,
             categories: categories,
             category: '',
             content: '',
@@ -37,7 +40,7 @@ export default class PostBox extends Component<PostBoxProps, PostBoxState>{
             ref: firebase.firestore().collection('Posts'),
             profileImage: null
         }
-
+        this.fileUpload = null;
         firebase.auth().onAuthStateChanged((user: firebase.User | null) => {
             if(user){
                 this.setState({user: user});
@@ -63,9 +66,53 @@ export default class PostBox extends Component<PostBoxProps, PostBoxState>{
         Object.keys(payload).forEach(key => {if(payload[key as keyof typeof payload] === null || payload[key as keyof typeof payload] === undefined) delete payload[key as keyof typeof payload]});
         this.state.ref.add(payload);
     }
+    handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) =>{
+        var files = event.target.files;
+        if(files){
+            var file = files[0];
+            var storage = storageRef.child(`post-images/${uuid()}-${file.name}`);
+            storage.put(file).then((snapshot: firebase.storage.UploadTaskSnapshot) => {
+                snapshot.ref.getDownloadURL().then((downloadURL) => {
+                    this.setState({image_url: downloadURL as string});
+                }).then(this.handleSubmitImage);
+            });
+        }
+    }
+    handleUploadButton = () => {
+        this.fileUpload?.click();
+    }
+    handleSubmitImage = (event: React.FormEvent<HTMLFormElement> | void) => {
+        if(event)
+            event.preventDefault();
+       this.setState({openImageModal: false})
+    }
+
     render(){ 
         return(
         <>
+           <Modal aria-labelledby="modal-title"
+                open={this.state.openImageModal}
+                onClose={()=>this.setState({openImageModal: false})}>
+                    <div className={styles.modal}>
+                        <h1 id="modal-title">Upload Image</h1>
+                        <input ref={input => this.fileUpload = input} type="file" style={{display: 'none'}} onChange={this.handleFileUpload} accept="image/*"/>
+                        <Button className={styles.button} onClick={this.handleUploadButton}>upload</Button>
+                        <hr className={styles.hr} data-content="or"/>
+                        <form onSubmit={this.handleSubmitImage}>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            fullWidth
+                            id="image-url"
+                            label="Image URL"
+                            name="imageURL"
+                            autoComplete="imageURL"
+                            autoFocus
+                            onChange={e=>this.setState({image_url: e.currentTarget.value})}
+                             /> 
+                        </form>
+                    </div>
+            </Modal>
             <div className={styles.card}>
                 <div className={styles.upper}>
                     <div id="profile">
@@ -79,6 +126,7 @@ export default class PostBox extends Component<PostBoxProps, PostBoxState>{
                         placeholder="Enter content"
                         onChange={(event)=>this.setState({content: event.target.value})}
                         />
+                        {(!this.state.openImageModal&&this.state.image_url)&&<img className={styles.postboxImage} src={this.state.image_url} alt="user uploaded content"/>}
                         <div className={`${styles.characterCount} ${(this.state.content && this.state.content?.length > 200)?styles.danger:(this.state.content && this.state.content?.length > 100)?styles.warning:''}`}>{this.state.content?.length}/250 characters</div>
                     </div>
                 </div>
@@ -93,7 +141,7 @@ export default class PostBox extends Component<PostBoxProps, PostBoxState>{
                 <IconButton aria-label="Insert Chart">
                     <InsertChartIcon/>
                 </IconButton>
-                <IconButton aria-label="Attach image">
+                <IconButton aria-label="Attach image" onClick={()=> this.setState({openImageModal: true})}>
                     <ImageIcon/>
                 </IconButton>
                 <span className={styles.gap}></span>
