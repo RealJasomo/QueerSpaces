b23d9aed0f07e353d6a26502079a897504b29e04
realjasomo <jason.cramer6@gmail.com>
2021-02-14 06:12:07 -0500

    finished poll creator

--- queer-spaces/src/components/cards/PostBox.tsx
+++ queer-spaces/src/components/cards/PostBox.tsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
-import {IconButton, TextField , Button, Modal} from '@material-ui/core'
+import {IconButton, TextField , Button, Modal, InputAdornment, OutlinedInput} from '@material-ui/core'
 import Autocomplete from '@material-ui/lab/Autocomplete'
 import BlankProfile from '../../res/bp.png'
 import styles from '../../css/postbox.module.css'
 import InsertChartIcon from '@material-ui/icons/InsertChart'
 import ImageIcon from '@material-ui/icons/Image'
 import SendIcon from '@material-ui/icons/Send'
+import AddBoxIcon from '@material-ui/icons/AddBox'
+import RemoveCircleIcon from '@material-ui/icons/RemoveCircle'
+import SaveIcon from '@material-ui/icons/Save'
+import DeleteIcon from '@material-ui/icons/Delete'
 import {firebase, storageRef} from '../';
 import {categories} from '../../util/Constants'
 import {v4 as uuid} from 'uuid'
@@ -16,6 +20,7 @@ interface PostBoxProps {
 
 interface PostBoxState {
     openImageModal: boolean,
+    openPollCreator: boolean,
     categories: Array<String>,
     category: String | null,
     content: string | null,
@@ -24,7 +29,8 @@ interface PostBoxState {
     image_url?: string | null,
     user: firebase.User | null,
     ref: firebase.firestore.CollectionReference<firebase.firestore.DocumentData>,
-    profileImage: string | null
+    profileImage: string | null,
+    hasPoll: boolean
 }
 
 export default class PostBox extends Component<PostBoxProps, PostBoxState>{
@@ -33,12 +39,14 @@ export default class PostBox extends Component<PostBoxProps, PostBoxState>{
         super(props);
         this.state = {
             openImageModal: false,
+            openPollCreator: false,
             categories: categories,
             category: '',
             content: '',
             user: null,
             ref: firebase.firestore().collection('Posts'),
-            profileImage: null
+            profileImage: null,
+            hasPoll: false
         }
         this.fileUpload = null;
         firebase.auth().onAuthStateChanged((user: firebase.User | null) => {
@@ -58,13 +66,22 @@ export default class PostBox extends Component<PostBoxProps, PostBoxState>{
             category: this.state.category,
             text_content: this.state.content,
             poll_question: this.state.poll_question,
-            poll_options: this.state.poll_options,
             image_url: this.state.image_url,
             user_id: this.state.user?.uid,
             created: firebase.firestore.Timestamp.now()
         }
         Object.keys(payload).forEach(key => {if(payload[key as keyof typeof payload] === null || payload[key as keyof typeof payload] === undefined) delete payload[key as keyof typeof payload]});
-        this.state.ref.add(payload);
+        this.state.ref.add(payload)
+        .then((doc)=>{
+            if(this.state.hasPoll){
+                var pollCollection = doc.collection('pollOptions');
+                this.state.poll_options?.forEach(v => {
+                    pollCollection.add({
+                        option_text:v 
+                    })
+                })
+            }
+        }).then(()=>{this.setState({hasPoll: false, poll_options:[], poll_question: ''})});
     }
     handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) =>{
         var files = event.target.files;
@@ -85,6 +102,39 @@ export default class PostBox extends Component<PostBoxProps, PostBoxState>{
         if(event)
             event.preventDefault();
        this.setState({openImageModal: false})
+    }
+    handlePollState = (i: number) => (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        var arr = [];
+        if(this.state.poll_options){
+            arr = [...this.state.poll_options];
+            console.log(`${arr} and ${i}`)
+            if(i < arr.length)
+                arr[i] = event.currentTarget.value;
+            else
+                arr.push(event.currentTarget.value);
+            }else{
+            arr.push(event.currentTarget.value);
+         }
+         this.setState({poll_options: arr});
+         console.log(this.state.poll_options)
+    }
+
+    handleNewPollOption = () => {
+        if(this.state.poll_options && (this.state.poll_options.length > 4))
+            return;
+        var arr: string[] = [];
+        if(this.state.poll_options)
+            arr = [...this.state.poll_options];
+        arr.push('');
+        this.setState({poll_options: arr});
+    }
+
+    handleRemoveOption = (i: number) => () => {
+        if(this.state.poll_options){
+            var arr = [...this.state.poll_options]
+            arr.splice(i,1);
+            this.setState({poll_options: arr});
+        }
     }
 
     render(){ 
@@ -113,6 +163,59 @@ export default class PostBox extends Component<PostBoxProps, PostBoxState>{
                         </form>
                     </div>
             </Modal>
+            <Modal aria-labelledby="poll-title"
+                open={this.state.openPollCreator}
+                onClose={()=>this.setState({openPollCreator: false})}>
+                <div className={styles.modal}>
+                    <h1 id="pool-title">Create a Poll</h1>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        fullWidth
+                        id="poll-question"
+                        label="Poll Question"
+                        name="pollQuestion"
+                        autoComplete="pollQuestion"
+                        autoFocus
+                        value={this.state.poll_question}
+                        onChange={e=>this.setState({poll_question: e.currentTarget.value})} />
+                    <h3 style={{textAlign:'left', margin:'15px', width:'100%'}}>Poll Options:</h3>
+                    {this.state.poll_options?.map((_, idx) => {
+                        return (<>
+                        <h6 style={{textAlign:'left', marginLeft:'15px', textDecoration:'italics'}}>Option {idx+1}</h6>
+                        <OutlinedInput
+                            key={idx}
+                            id={`poll-option-${idx+1}`}
+                            autoComplete="pollOption"
+                            fullWidth
+                            value={this.state.poll_options?this.state.poll_options[idx]:''}
+                            onChange={this.handlePollState(idx)}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                    aria-label="remove poll option"
+                                    edge="end"
+                                    onClick={this.handleRemoveOption(idx)}
+                                    >
+                                    <RemoveCircleIcon/>
+                                    </IconButton>
+                                </InputAdornment>
+                                }
+                        /></>)
+                    })}
+                    <div style={{display:'flex', justifyContent:"center"}}>
+                        <IconButton onClick={this.handleNewPollOption}>
+                                <AddBoxIcon style={{color: "#A42197", width:'2rem'}}/>
+                        </IconButton>
+                        <IconButton onClick={()=>{this.setState({openPollCreator: false, hasPoll: true})}}>
+                            <SaveIcon/>
+                        </IconButton>
+                        <IconButton onClick={()=>{this.setState({poll_question:null,poll_options:[],hasPoll:false, openPollCreator: false})}}>
+                            <DeleteIcon />
+                        </IconButton>
+                    </div>
+                </div>
+            </Modal>
             <div className={styles.card}>
                 <div className={styles.upper}>
                     <div id="profile">
@@ -138,8 +241,8 @@ export default class PostBox extends Component<PostBoxProps, PostBoxState>{
                 renderInput={(params) => <TextField {...params} label="Category" />}
                 onChange={(_, val)=>this.setState({category: val})}
                 />
-                <IconButton aria-label="Insert Chart">
-                    <InsertChartIcon/>
+                <IconButton aria-label="Insert Chart" onClick={()=>this.setState({openPollCreator: true})}>
+                    <InsertChartIcon style={{color:(this.state.hasPoll)?'#A42197':'inherit'}}/>
                 </IconButton>
                 <IconButton aria-label="Attach image" onClick={()=> this.setState({openImageModal: true})}>
                     <ImageIcon/>
